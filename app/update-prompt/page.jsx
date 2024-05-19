b9ac0f51dd6b18b776aa8e97fa4943652bd0ec95
HeinThetZaw22 <heinthetzaw2212@gmail.com>
2024-05-19 21:49:02 +0630

    reder case

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,66 +1,90 @@
 "use client";
-import {useState, useEffect} from 'react';
-// import {useSession} from 'next-auth/react';
-import Form from '../../components/Form';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { toast } from 'sonner';
-
+import { useState, useEffect, Suspense} from "react";
+import Form from "../../components/Form";
+import { useRouter, useSearchParams } from "next/navigation";
+import { toast } from "sonner";
 
 const EditPrompt = () => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const promptId = searchParams.get('id');
-    const [submitting, setSubmitting] = useState(false);
-    // const {data: session} = useSession();
-    const [post, setPost] = useState({
-        prompt: '',
-        tag: ''
-    })
-    console.log(promptId);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+  // console.log(promptId);
 
-    useEffect(()=>{
-        const getPromptDetail = async () => {
-            const response = await fetch(`/api/prompt/${promptId}`);
-            const data = await response.json();
-            console.log(data);
-            setPost({
-                prompt: data.prompt,
-                tag: data.tag
-            })
+  useEffect(() => {
+    const getPromptDetail = async () => {
+      if(!promptId) return ;
+      try {
+        const response = await fetch(`/api/prompt/${promptId}`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch prompt details");
         }
-        if(promptId) getPromptDetail();
-    },[promptId])
-    const editPrompt = async (e) => {
-        e.preventDefault();
-        setSubmitting(true);
-        if(!promptId) return alert('Prompt Id not found');
-        try {
-            const response = await fetch(`/api/prompt/${promptId}`, {
-                method: 'PATCH',
-                body: JSON.stringify({
-                    prompt: post.prompt,
-                    tag: post.tag,
-                })
-            })
-            if(response.ok){
-                router.push('/');
-                toast.success("Prompt edited successfully")
-            }
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setSubmitting(false);
-        }
+        const data = await response.json();
+        setPost({
+          prompt: data.prompt,
+          tag: data.tag,
+        });
+      } catch (error) {
+        console.error("An error occurred while fetching the prompt details:", error);
+        toast.error("Failed to load prompt details");
+      }
+
+      // const response = await fetch(`/api/prompt/${promptId}`);
+      // const data = await response.json();
+      // // console.log(data);
+      // setPost({
+      //   prompt: data.prompt,
+      //   tag: data.tag,
+      // });
+
+    };
+    if (promptId) getPromptDetail();
+  }, [promptId]);
+  const editPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    if (!promptId){
+      console.error("Prompt Id not found");
     }
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+        toast.success("Prompt edited successfully");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+  // if (!promptId) {
+  //   return <div>Loading...</div>;
+  // }
   return (
-    <Form
-     type="Edit"
-     post={post}
-     setPost={setPost}
-     submitting={submitting}
-     handleSubmit={editPrompt}
+    <Suspense fallback={<>loading...</>}>
+      <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={editPrompt}
     />
-  )
-}
+    </Suspense>
+    
+  );
+  
+};
 
-export default EditPrompt
+export default EditPrompt;
